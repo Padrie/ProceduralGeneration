844924d245f3a25ba453cc5ca6b52147c0698f9a
Padrie <pstrehlow007@gmail.com>
2024-04-26 09:50:17 +0200

    Basic Diffusion-Limited Aggregation implemented

--- Assets/+++Workdata/Scripts/DiffusionLimitedAggregation.cs
+++ Assets/+++Workdata/Scripts/DiffusionLimitedAggregation.cs
@@ -10,12 +10,36 @@ public class DiffusionLimitedAggregation : MonoBehaviour
 {
     public List<GameObject> tree = new List<GameObject>();
     public Vector2 canvas;
-    public int steps;
+    public int walkers;
+
+    [Header("Spawn Walkers From")] public bool top;
+    public bool bottom;
+    public bool left;
+    public bool right;
+    [Space(10)] public int failSafe = 0;
+    public int maxLoops = 10000;
 
     [ButtonMethod]
     public void Generate()
     {
         Dictionary<Vector3, GameObject> positions = new Dictionary<Vector3, GameObject>();
+
+        Vector3 topLeft = new();
+        Vector3 topRight = new();
+        Vector3 bottomLeft = new();
+        Vector3 bottomRight = new();
+
+        if (top || left)
+            topLeft = new Vector3(-canvas.x, 0, canvas.y);
+        if (top || right)
+            topRight = new Vector3(canvas.x, 0, canvas.y);
+        if (bottom || left)
+            bottomLeft = new Vector3(-canvas.x, 0, -canvas.y);
+        if (bottom || right)
+            bottomRight = new Vector3(canvas.x, 0, -canvas.y);
+
+        print(topRight);
+        print(topLeft);
 
         var a = GameObject.CreatePrimitive(PrimitiveType.Cube);
         a.transform.parent = transform;
@@ -23,17 +47,18 @@ public class DiffusionLimitedAggregation : MonoBehaviour
 
         bool stuck = false;
 
-        for (int i = 0; i < steps; i++)
+        for (int i = 0; i < walkers; i++)
         {
             GameObject walker = GameObject.CreatePrimitive(PrimitiveType.Cube);
             stuck = false;
 
-            walker.transform.position = new Vector3(Random.Range(-canvas.x / 2, canvas.y / 2), 0,
-                Random.Range(-canvas.x / 2, canvas.y / 2));
+            walker.transform.position = SpawnWalker(topLeft, topRight);
 
             walker.transform.parent = transform;
-            while (!stuck)
+
+            while (!stuck && failSafe < maxLoops)
             {
+                failSafe++;
                 for (int j = 0; j < tree.Count; j++)
                 {
                     var d = Vector3.Distance(walker.transform.position, tree[j].transform.position);
@@ -45,13 +70,12 @@ public class DiffusionLimitedAggregation : MonoBehaviour
                     }
                 }
 
-                if (stuck)
-                    break;
+                if (stuck) break;
 
-                var vel = new Vector3((int)Random.Range(-canvas.x, canvas.x), 0,
-                    (int)Random.Range(-canvas.y, canvas.y));
+                var walkerPos = walker.transform.position;
 
-                walker.transform.position = vel;
+                walker.transform.position =
+                    Vector3.ClampMagnitude(new Vector3(walkerPos.x + Move().x, 0, walkerPos.z + Move().y), canvas.x);
             }
 
             tree.Add(walker);
@@ -67,8 +91,29 @@ public class DiffusionLimitedAggregation : MonoBehaviour
             else
                 positions.Add(position, obj);
         }
-            
+
         tree.RemoveAll(obj => obj == null);
+    }
+
+    public Vector3 SpawnWalker(Vector3 first, Vector3 second)
+    {
+        return new Vector3(Random.Range(first.x, first.z), 0, Random.Range(second.x, second.z));
+    }
+
+    Vector2 Move()
+    {
+        int randomDir = Random.Range(1, 5);
+        Vector2 dir = new Vector2(0, 0);
+
+        if (randomDir == 1)
+            dir = new Vector2(1, 0);
+        else if (randomDir == 2)
+            dir = new Vector2(-1, 0);
+        else if (randomDir == 3)
+            dir = new Vector2(0, 1);
+        else if (randomDir == 4)
+            dir = new Vector2(0, -1);
+        return dir;
     }
 
     [ButtonMethod]
--- Assets/+++Workdata/Scripts/RandomlyMoveCube.cs
+++ Assets/+++Workdata/Scripts/RandomlyMoveCube.cs
@@ -1,15 +1,63 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MyBox;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class RandomlyMoveCube : MonoBehaviour
 {
+    public Vector2 canvas;
     public GameObject cube;
-    public int count = 100;
-    public int moveDir = 1;
     public bool update = true;
+    public int steps;
+    
+    [Header("Spawn Walkers From")]
+    public bool top;
+    public bool bottom;
+    public bool left;
+    public bool right;
+
+    [ButtonMethod]
+    public void Bounds()
+    {
+        Vector3 topLeft = new();
+        Vector3 topRight = new();
+        Vector3 bottomLeft = new();
+        Vector3 bottomRight = new();
+        
+        if (top || left)
+            topLeft = new Vector3(-canvas.x, 0, canvas.y);
+
+        if (top || right)
+            topRight = new Vector3(canvas.x, 0, canvas.y);
+
+        if (bottom  || left)
+            bottomLeft = new Vector3(-canvas.x, 0, -canvas.y);
+
+        if (bottom || right)
+            bottomRight = new Vector3(canvas.x, 0, -canvas.y);
+
+        if(top)
+            for (int i = 0; i < steps; i++)
+                SpawnWalker(topLeft, topRight, transform);
+        if(bottom)
+            for (int i = 0; i < steps; i++)
+                SpawnWalker(bottomRight, bottomLeft, transform);
+        if(left)
+            for (int i = 0; i < steps; i++)
+                SpawnWalker(bottomLeft, topLeft, transform);
+        if(right)
+            for (int i = 0; i < steps; i++)
+                SpawnWalker(topRight, bottomRight, transform);
+    }
+
+    public void SpawnWalker(Vector3 first, Vector3 second, Transform parent)
+    {
+        GameObject walker = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        walker.transform.position = new Vector3(Random.Range(first.x, first.z), 0, Random.Range(second.x, second.z));
+        walker.transform.parent = parent;
+    }
 
     private void Awake()
     {
@@ -20,15 +68,27 @@ public class RandomlyMoveCube : MonoBehaviour
     {
         while (update)
         {
-            int moveDir = Random.Range(-1, 1);
-            
             var cubePos = cube.transform.position;
 
-            int moveDirX = Random.Range(-moveDir, moveDir * 2);
-            int moveDirY = Random.Range(-moveDir, moveDir * 2);
-
-            cube.transform.position = new Vector3(cubePos.x + moveDirX, 0, cubePos.z + moveDirY);
-            yield return null;
+            cube.transform.position = new Vector3(cubePos.x + Move().x, 0, cubePos.z + Move().y);
+            yield return new WaitForSeconds(0.2f);
         }
+    }
+
+    Vector2 Move()
+    {
+        int randomDir = Random.Range(1, 5);
+        Vector2 dir = new Vector2(0, 0);
+
+        if (randomDir == 1)
+            dir = new Vector2(1, 0);
+        else if (randomDir == 2)
+            dir = new Vector2(-1, 0);
+        else if (randomDir == 3)
+            dir = new Vector2(0, 1);
+        else if (randomDir == 4)
+            dir = new Vector2(0, -1);
+
+        return dir;
     }
 }
